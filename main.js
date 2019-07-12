2e4b3707033282a93fd2e737b98de70729572d58
emeaexe <39465402+emeaexe@users.noreply.github.com>
2019-07-12 17:25:52 -0300

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var myloc = window.location.href;
 var locarray = myloc.split("/");
 delete locarray[(locarray.length-1)];
 var arraytext = locarray.join("/");
-var arraytext = arraytext + "items/"
+var arraytext = arraytext + "fondo/"
 console.log("IT WORKS.")
 console.log(arraytext);
+
+$('document').ready(function(){
+	var bckImg = "url(" + arraytext + "a.png)"
+	document.body.style.backgroundImage = bckImg;
+});
