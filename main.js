dca61795f53dbfcc8f765ba13a43a0ada3cdad43
emeaexe <39465402+emeaexe@users.noreply.github.com>
2019-07-12 16:44:14 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var myloc = window.location.href;
 var locarray = myloc.split("/");
 delete locarray[(locarray.length-1)];
-var locarray2 = new Array(locarray, "fondo/");
 var arraytext = locarray.join("/");
+var arraytext = arraytext + "items/"
 console.log("IT WORKS.")
 console.log(arraytext);
